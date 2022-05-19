c665f29e008ccb6f0b9c6dc6ce85664d4fa09276
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-19 11:07:05 +0200

    felche bas cre

---
+++ src/components/icons/Flechebas.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg
+  width="12"
+  height="8"
+  viewBox="0 0 12 8"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <path
+    d="M1.33337 1.66666L6.00004 6.33332L10.6667 1.66666"
+    stroke="#111827"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
